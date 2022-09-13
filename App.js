9d27ee0de758ace971fbb40d607921721fcc6b8c
talhamujahid52 <55655696+talhamujahid52@users.noreply.github.com>
2022-09-13 12:37:24 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>My App</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
